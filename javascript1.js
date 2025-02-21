3ed2f54dfd0549524d13d44c5b3ef8a95482a0fb
[Adeeb Ahmed] <adeebansari1310@gmail.com>
2025-02-21 16:50:04 +0500

    js1 commit

---
+++ javascript1.js
@@ -0,0 +1,64 @@
+alert ("Error! please enter a Valid Password")
+
+alert ("welcome to Websites...")
+
+alert ("Javasript CoDing..-")
+
+var age=prompt("Your Age")
+  console.log(age)
+  alert('your age' + age)
+
+
+ var message = 'Hello';
+ console.log(message)
+
+
+ var firstname = 'Adeeb';
+ var second_name = 'Ahmed'
+
+console.log(firstname)
+console.log(second_name)
+
+var $thirdname = 'Adeeb';
+var fourth_name = 'Ahmed'
+
+ console.log($thirdname+fourth_name)
+
+var name = 'jameel Ahmed';
+
+console.log(name)
+console.log(typeof name);
+
+ var marks= 1020+30;
+console.log (marks)
+console.log (typeof marks)
+
+ var pass= true;
+ console.log (pass)
+console.log (typeof pass)
+
+ var y =null;
+ console.log(typeof y)
+
+
+var a = 20;
+var b = "50";
+var c = "Hazel";
+console.log(a + b);
+console.log(a * b); 
+console.log(a * c); 
+console.log(a / b);
+
+var f = 25;
+var g =32;
+
+console.log(32-25)
+
+var h = 17;
+var i = 80;
+
+console.log(h/i)
+
+
+
+
